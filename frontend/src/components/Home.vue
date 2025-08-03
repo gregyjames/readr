28598c918133d81e58c6d1a1bfa770bfa1c47297
Greg James <gregvjames99@gmail.com>
2025-08-02 23:06:37 -0500

    Add icons for list/card, and border

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
+import CardIcon from '../assets/card.svg'
+import ListIcon from '../assets/list.svg'
 
 interface Article {
   ID: number
@@ -23,18 +25,22 @@ onMounted(async () => {
 <template>
   <div>
     <div class="flex pt-20 justify-between items-center mb-6">
-      <h2 class="text-2xl font-bold text-gray-800 dark:text-white">{{ msg }}</h2>
-      <button
-        @click="viewMode = viewMode === 'card' ? 'list' : 'card'"
-        class="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition"
-      >
-        Switch to {{ viewMode === 'card' ? 'List' : 'Card' }} View
+      <button @click="viewMode = viewMode === 'card' ? 'list' : 'card'"
+        class="px-3 py-2 flex items-center gap-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition">
+        <span v-if="viewMode === 'card'">
+          <!-- List icon to indicate switch -->
+          <ListIcon class="w-6 h-6 text-white"/>
+        </span>
+        <span v-else>
+          <!-- Card icon to indicate switch -->
+          <CardIcon class="w-6 h-6 text-white"/>
+        </span>
       </button>
     </div>
 
     <!-- Card View -->
     <div v-if="viewMode === 'card'" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      <div v-for="article in articles" :key="article.ID" class="flex-1 bg-white rounded-xl shadow-md overflow-hidden">
+      <div v-for="article in articles" :key="article.ID" class="flex-1 bg-white rounded-xl shadow-md overflow-hidden border border-gray-500 ">
         <div class="md:flex h-full">
           <div class="w-full md:w-48 aspect-square flex-shrink-0">
             <img :src="article.image" alt="Cover" class="w-full h-full object-cover" />
@@ -56,7 +62,7 @@ onMounted(async () => {
 
     <!-- List View -->
     <div v-else class="space-y-4">
-      <div v-for="article in articles" :key="article.ID" class="flex items-center bg-white rounded-xl shadow-sm p-4">
+      <div v-for="article in articles" :key="article.ID" class="flex items-center bg-white rounded-xl shadow-sm p-4 border border-gray-500 ">
         <img :src="article.image" alt="Cover" class="w-24 h-24 object-cover rounded-md mr-4" />
         <div>
           <router-link
